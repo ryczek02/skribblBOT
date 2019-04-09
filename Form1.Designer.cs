fa26e89c9d7bff0a1c350525ac6b8b0b67e7ba26
ryczek02 <49419559+ryczek02@users.noreply.github.com>
2019-04-09 02:17:29 +0200

    First commit
    version 0.1a

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace skribblBOT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(619, 471);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Wybierz grafikę";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(537, 392);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Przeglądaj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 394);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(524, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(606, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Rysuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(606, 366);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 491);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(613, 23);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(639, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(257, 501);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Konsola";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 465);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Aktywuj narzędzia testowe";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Location = new System.Drawing.Point(7, 20);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(244, 403);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 523);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Załadowano program.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(639, 521);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rysunek: ";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 429);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(244, 30);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "O programie";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 548);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Skribbl.io BOT";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using Microsoft.Test.Input;
+namespace skribblBOT
+{
+    public partial class Form1 : Form
+    {
+        public string FILE_NAME;
+        public int rysunek = 1; //dodawanie do labela numeru rysunku 
+        private const double BW_THRESHOLD = 0.5; 
+        private readonly Color colorBlack =
+          Color.FromArgb(255, 0, 0, 0); //detekcja czarnego
+        private readonly Color colorWhite =
+          Color.FromArgb(255, 255, 255, 255); //detekcja białego
+        public Bitmap originalImage; //inicjowanie oryginalnego obrazka
+        public Bitmap convertedImage; //inicjowanie przekonwertowanego obrazka
+        private readonly List<Vertex> vertices = new List<Vertex>(); //lista wierzchołków
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.Text = "skribblBOT.exe\nZostał pomyślnie aktywowany.\n==========\nZanim przejdziesz do rysowania, odpal okno przeglądarki pod programem. Pamiętaj, aby wyłączyć program podczas rysowania użyj Ctrl+Alt+Delete i zabij proces.\n==========\nGOTOWY."; //tekst w konsoli przy odpaleniu programu
+        }
+
+        public Bitmap Img2BW(Bitmap imgSrc, double threshold)
+        {
+            int width = imgSrc.Width;
+            int height = imgSrc.Height;
+            Color pixel;
+            Bitmap imgOut = new Bitmap(imgSrc);
+            label1.Text = "Konwertowanie obrazu...";
+            progressBar1.Value = 0; //progressbar  ktory nie jest  uzywany mozna  do niego dopisac jakas funkcje
+            for (int row = 0; row < height - 1; row++) //konwertowanie  obrazka
+            {
+                for (int col = 0; col < width - 1; col++)
+                {
+                    pixel = imgSrc.GetPixel(col, row);
+                    if (pixel.GetBrightness() < threshold)
+                    {
+                        this.vertices.Add(new Vertex(col, row));
+                        imgOut.SetPixel(col, row, this.colorBlack);
+                    }
+                    else
+                    {
+                        imgOut.SetPixel(col, row, this.colorWhite);
+                    }
+                }
+            } //koniec konwertowania
+            label1.Text = "Zakończono konwertowanie..."; //komunikat
+            richTextBox1.AppendText("\nBłąd, nie podano ścieżki"); //wpis  do konsoli
+            return imgOut;
+        }
+    private void button1_Click(object sender, EventArgs e)
+        {
+            if (FILE_NAME == null)
+            {
+                richTextBox1.AppendText("\nBłąd, nie podano ścieżki"); //komunikat do konsoli
+            }
+            else
+            {
+                Mouse.MoveTo(new Point(1700, 400)); //proces rysowania zautomatyzowaną myszką, ustawienie myszki etc.
+                Mouse.Click(MouseButton.Left);
+                Mouse.MoveTo(new Point(875, 128)); //punkty do klikania w paincie na  czarny na rozdzialce 1920x1080
+                Mouse.Click(MouseButton.Left);
+                label1.Text = "Rysowanie...";
+                rysunek++;
+                label2.Text = "Rysunek: " + rysunek; //komunikat liczący rysunki
+                foreach (Vertex vert in this.vertices)
+                {
+                    string[] sizeParts = vert.ToString().Split(':'); //rysowanie przekonwertowanego obrazka
+                    int height = int.Parse(sizeParts[0]);
+                    int width = int.Parse(sizeParts[1]);
+                    Size mySize = new Size(height, width);
+                    Mouse.MoveTo(new Point(height + 3, width + 3));
+                    Mouse.Click(MouseButton.Left);
+                    richTextBox1.AppendText("=="); //bez tego program za szybko dziala  i go wykurwia wiem ze moglem uzyc sleepa ale  po co pozdro
+                }
+                vertices.Clear();
+                label1.Text = "Zakończono rysowanie...";
+                richTextBox1.AppendText("\nGOTOWE."); //komunikaty
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jeszcze nie wiem co to będzie robiło", "Jebać sąd"); // xD
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog //dialog otwieranie pliku
+            {
+                InitialDirectory = @"C:\",
+                Title = "Przeglądaj pliki graficzne",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "jpg",
+                Filter = "Pliki graficzne (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) //na  wypadek klikniecia  ok w dialogu
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                FILE_NAME = openFileDialog1.FileName;
+                pictureBox1.ImageLocation = FILE_NAME;
+                originalImage = new Bitmap(FILE_NAME);
+                convertedImage =
+                  this.Img2BW(this.originalImage, BW_THRESHOLD);
+                vertices.Clear(); //czyszczenie listy z wierzchołkami
+            }
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Program został napisany w C#, użyłem do niego bibliotek do testowania programów prosto od Microsoftu.", "O programie...");
+        }
+    }
+
+    public class Vertex
+    {
+        public Vertex(int i, int j)
+        {
+            //koordynaty lewego gornego rogu pola rysowania
+            this.X = i+483; 
+            this.Y = j+217;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string ToString()
+        {
+            return string.Format("{0}:{1}", this.X, this.Y);  //zwracanie  puntku
+        }
+    }
+}
